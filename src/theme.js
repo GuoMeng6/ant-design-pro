5e863b17625b7185a541c747a71b096d5dfe9403
Hsdiana <2831103860@qq.com>
2018-08-02 09:31:30 +0800

    btn

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,8 @@
 // https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
 module.exports = {
   'primary-color': '#A6D6D0',
-  'border-radius-base': '20px',
-  'border-radius-sm': '10px',
+  // 'border-radius-base': '20px',
+  // 'border-radius-sm': '10px',
   'text-color': '#35536C',
   'text-color-secondary': '#35536C',
   'heading-color': '#35536C',
